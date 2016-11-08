4b03d62b3ab75203865e3053d1e2f1a6d0d390a4
lazamar <lazaronijunior@gmail.com>
2016-11-08 08:45:15 +0000

    Add SSL support

--- index.js
+++ index.js
@@ -3,13 +3,20 @@
 // Libraries
 const express = require('express');
 const app = express();
-const http = require('http').Server(app); // eslint-disable-line new-cap
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/lazamar.co.uk/privkey.pem'),
+  cert: fs.readFileSync('test/fixtures/keys/cert.pem'),
+};
 
 const PORT = 80;
 
 // Serve static files from root
 app.use(express.static('static'));
 
-http.listen(PORT, () => {
+
+https.createServer(options, app).listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
 });
