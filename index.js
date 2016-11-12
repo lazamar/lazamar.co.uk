b83588d40e07c11117baa9b557bfb8bd334f9733
lazamar <lazaronijunior@gmail.com>
2016-11-12 16:41:14 +0000

    Add option to run in production and development modes

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
 /* eslint-env node */
 
+// Load environment variables
+require('dotenv').config();
+
 // Libraries
 const fs = require('fs');
 const http = require('http');
 const https = require('https');
-const privateKey = fs.readFileSync('./.ssl/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('./.ssl/cert.pem', 'utf8');
-
-const credentials = { key: privateKey, cert: certificate };
 const express = require('express');
+
+
 const app = express();
 
-// your express configuration here
+if (process.env.NODE_ENV !== 'development') {
+  // Start HTTPS server
+  const privateKey = fs.readFileSync('./.ssl/privkey.pem', 'utf8');
+  const certificate = fs.readFileSync('./.ssl/cert.pem', 'utf8');
+  const credentials = { key: privateKey, cert: certificate };
+  const httpsServer = https.createServer(credentials, app);
 
-const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
+  httpsServer.listen(443, () => console.log(`
+    HTTPS listening on port 443
+    Redirecting HTTP requests to HTTPS endpoints
+  `));
 
-httpServer.listen(80, () => console.log('HTTP listening on port 80'));
-httpsServer.listen(443, () => console.log('HTTPS listening on port 443'));
+  // Redirect all HTTP requests to HTTPS
+  http.createServer((req, res) => {
+    res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
+    res.end();
+  }).listen(80);
+} else {
+  const httpServer = http.createServer(app);
+  httpServer.listen(80, () => console.log(`
+    HTTP listening on port 80
+    Not running HTTPS server
+  `));
+}
+
 
 app.use(express.static('./static'));
