7f09916d6e69f5df59ec599dbb4a442b9be68a47
lazamar <lazaronijunior@gmail.com>
2016-11-12 18:09:08 +0000

    Fix package.json and silver-magpie-backend install script

--- scripts/update-silver-magpie.sh
+++ scripts/update-silver-magpie.sh
@@ -1,5 +1,5 @@
-npm install silver-magpie-backend
-&& cd node_modules/silver-magpie-backend
-&& npm install
+npm install silver-magpie-backend &&
+cd node_modules/silver-magpie-backend &&
+npm install
 
 cd ../..
